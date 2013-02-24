59b12bfac2da6b895ea7596ddfa7e0df0870d7af
Andrey Konovalov <adech.fo@gmail.com>
2013-02-25 00:56:29 +0400

    Macros fixed.

--- include/enet-plus/base/macros.hpp
+++ include/enet-plus/base/macros.hpp
@@ -7,34 +7,42 @@
 // The CHECK() macro is used for checking assertions, and will cause
 // an immediate crash if its assertion is not met. DCHECK() is like
 // CHECK() but is only compiled in on debug builds.
-#define CHECK(x)                                                  \
-  do {                                                            \
-    if(!(x)) {                                                    \
-      fprintf(stderr, "Assertion failed: %s, file %s, line %u\n", \
-        #x, __FILE__, __LINE__);                                  \
-      abort();                                                    \
-    }                                                             \
-  } while(0)
+#ifndef CHECK
+  #define CHECK(x)                                                  \
+    do {                                                            \
+      if(!(x)) {                                                    \
+        fprintf(stderr, "Assertion failed: %s, file %s, line %u\n", \
+          #x, __FILE__, __LINE__);                                  \
+        abort();                                                    \
+      }                                                             \
+    } while(0)
+#endif
 
-#ifdef NDEBUG
-# define DCHECK(x)
-#else
-# define DCHECK(x) CHECK(x)
+#ifndef DCHECK
+  #ifdef NDEBUG
+    #define DCHECK(x)
+  #else
+    #define DCHECK(x) CHECK(x)
+  #endif
 #endif
 
 // The SCHECK() macro is used for checking compile-time assertions,
 // and will cause a compilation error if its assertion is not met.
-template<bool x> struct __SCHECK_F;
-template<      > struct __SCHECK_F <true> {};
-template<int  x> struct __SCHECK_P        {};
-#define SCHECK(B) \
-  typedef __SCHECK_P<sizeof(__SCHECK_F<((B)? true : false)>)> __SCHECK_ASSERT##__LINE__
+#ifndef SCHECK
+  template<bool x> struct __SCHECK_F;
+  template<      > struct __SCHECK_F <true> {};
+  template<int  x> struct __SCHECK_P        {};
+  #define SCHECK(B) \
+    typedef __SCHECK_P<sizeof(__SCHECK_F<((B)? true : false)>)> __SCHECK_ASSERT##__LINE__
+#endif
 
 // A macro to disallow the copy constructor and operator= functions.
 // This should be used in the private: declarations for a class.
-#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
-  TypeName(const TypeName&);               \
-  void operator=(const TypeName&)
+#ifndef DISALLOW_COPY_AND_ASSIGN
+  #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
+    TypeName(const TypeName&);               \
+    void operator=(const TypeName&)
+#endif
 
 // A macro to disallow all the implicit constructors, namely the
 // default constructor, copy constructor and operator= functions.
@@ -42,8 +50,10 @@ template<int  x> struct __SCHECK_P        {};
 // This should be used in the private: declarations for a class
 // that wants to prevent anyone from instantiating it. This is
 // especially useful for classes containing only static methods.
-#define DISALLOW_IMPLICIT_CONSTRUCTORS(TypeName) \
-  TypeName();                                    \
-  DISALLOW_COPY_AND_ASSIGN(TypeName)
+#ifndef DISALLOW_IMPLICIT_CONSTRUCTORS
+  #define DISALLOW_IMPLICIT_CONSTRUCTORS(TypeName) \
+    TypeName();                                    \
+    DISALLOW_COPY_AND_ASSIGN(TypeName)
+#endif
 
 #endif // ENET_PLUS_BASE_MACROS_HPP_
